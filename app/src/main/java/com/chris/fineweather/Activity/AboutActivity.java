8bbdda255fb6ea572a533328bf6fa515207b72de
Chris <316730924@qq.com>
2017-03-18 11:15:15 +0800

    “生活建议”布局优化 & 修复关于页面版本号显示问题

--- app/src/main/java/com/chris/fineweather/Activity/AboutActivity.java
+++ app/src/main/java/com/chris/fineweather/Activity/AboutActivity.java
@@ -1,10 +1,13 @@
 package com.chris.fineweather.Activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.chris.fineweather.R;
 
@@ -21,16 +24,33 @@ public class AboutActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        //在AppBar上显示当前应用版本号
+        TextView versionText = (TextView) findViewById(R.id.version_text);
+        versionText.setText(getVersionName());
+
+    }
+
+    //获取应用版本号(versionName)
+    public String getVersionName() {
+        try {
+            PackageManager packageManager = getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+        public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    finish();
+            }
+
+            return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
